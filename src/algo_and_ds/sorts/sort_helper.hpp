3761fbd4c878bc6593549fa7c1827cc588d81a9e
Sinter Wong <sintercver@163.com>
2022-09-25 21:16:37 +0800

    add merge sort and insert sort

--- src/algo_and_ds/sorts/sort_helper.hpp
+++ src/algo_and_ds/sorts/sort_helper.hpp
@@ -1,10 +1,10 @@
 
 
 #include <array>
+#include <cassert>
 #include <chrono>
 #include <iostream>
 #include <iterator>
-#include <cassert>
 
 namespace algo_and_ds {
 namespace sort {
@@ -52,17 +52,16 @@ void generateNearlyOrderedArray(Container &container, int num) {
 }
 
 template <typename Container>
-void testSort(std::string const &name, void (*sort)(Container&),
+void testSort(std::string const &name, void (*sort)(Container &),
               Container &container) {
-
-  auto start = std::chrono::steady_clock::now();
+  using namespace std::chrono;
+  auto start = steady_clock::now();
   sort(container);
-  auto end = std::chrono::steady_clock::now();
+  auto end = steady_clock::now();
   assert(isSorted(container.cbegin(), container.cend()));
-  std::cout
-      << name << ": "
-      << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
-      << "ms" << std::endl;
+  float micro_s = duration_cast<microseconds>(end - start).count();
+  std::cout << name << ": " << micro_s / 1000 / 1000
+            << "s" << std::endl;
 }
 
 } // namespace sort
