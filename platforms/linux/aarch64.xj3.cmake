bbd1a63755aa2621c684b6836835d4e719c6553c
Sinter Wong <sintercver@gmail.com>
2022-10-15 19:00:20 +0800

    cross-complier for xj3

--- platforms/linux/aarch64.xj3.cmake
+++ platforms/linux/aarch64.xj3.cmake
@@ -3,27 +3,27 @@ MESSAGE(STATUS "Configure Cross Compiler")
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR arm)
 
-SET(CMAKE_C_COMPILER       arm-himix200-linux-gcc)
-SET(CMAKE_CXX_COMPILER     arm-himix200-linux-g++)
+SET(CMAKE_C_COMPILER       ${TOOLCHAIN_ROOTDIR}/bin/aarch64-linux-gnu-gcc)
+SET(CMAKE_CXX_COMPILER     ${TOOLCHAIN_ROOTDIR}/bin/aarch64-linux-gnu-g++)
 
 # set searching rules for cross-compiler
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-SET(YOUR_TARGET_OS linux)
-SET(YOUR_TARGET_ARCH armv8-a)
+SET(TARGET_OS linux)
+SET(TARGET_ARCH aarch64)
 
 SET(CMAKE_SKIP_BUILD_RPATH TRUE)
 SET(CMAKE_SKIP_RPATH TRUE)
 
 # set ${CMAKE_C_FLAGS} and ${CMAKE_CXX_FLAGS}flag for cross-compiled process
-#SET(CROSS_COMPILATION_ARM himix200)
-#SET(CROSS_COMPILATION_ARCHITECTURE armv7-a)
+SET(CROSS_COMPILATION_ARM xj3)
+SET(CROSS_COMPILATION_ARCHITECTURE aarch64)
 
 # set g++ param
 # -fopenmp link libgomp
-SET(CMAKE_CXX_FLAGS "-std=c++17 -march=armv8-a -mfloat-abi=softfp -mfpu=neon-vfpv4 \
+SET(CMAKE_CXX_FLAGS "-std=c++17 -march=aarch64 -mfloat-abi=softfp -mfpu=neon-vfpv4 \
     -ffunction-sections \
     -fdata-sections -O2 -fstack-protector-strong -lm -ldl -lstdc++\
     ${CMAKE_CXX_FLAGS}")
