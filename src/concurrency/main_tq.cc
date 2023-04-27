ca521ca76569f8fe3d429c3bd97bf8042a2ba487
Sinter Wong <sintercver@163.com>
2023-04-27 17:53:36 +0800

    finished threadsafe_queue

--- src/concurrency/main_tq.cc
+++ src/concurrency/main_tq.cc
@@ -1,5 +1,6 @@
 #include "logger/logger.hpp"
-#include "threadsafe_queue.hpp"
+// #include "threadsafe_queue.hpp"
+#include "threadsafe_queue_fg.hpp"
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -10,7 +11,8 @@ int main() {
 
   BasicLearningLoggerInit(true, true, true, true);
 
-  threadsafe_queue<int> myQueue;
+  // threadsafe_queue<int> myQueue;
+  threadsafe_queue_fg_done<int> myQueue;
 
   std::thread t1([&myQueue]() {
     for (size_t i = 0; i < 10; ++i) {
@@ -19,11 +21,15 @@ int main() {
   });
 
   std::thread t2([&myQueue]() {
-    BASIC_LOGGER_INFO("waitPopData: {}", *myQueue.wait_and_pop());
+    for (size_t i = 0; i < 3; ++i) {
+      BASIC_LOGGER_INFO("waitPopData: {}", *myQueue.wait_and_pop());
+    }
   });
 
   std::thread t3([&myQueue]() {
-    BASIC_LOGGER_INFO("tryPopData: {}", *myQueue.try_pop());
+    for (size_t i = 0; i < 7; ++i) {
+      BASIC_LOGGER_INFO("tryPopData: {}", *myQueue.try_pop());
+    }
   });
 
   t1.join();
--- src/concurrency/threadsafe_queue_fg.hpp
+++ src/concurrency/threadsafe_queue_fg.hpp
@@ -1,6 +1,7 @@
 #ifndef _BASIC_CONCUNNENCY_THREADSAFE_QUEUE_FG_HPP_
 #define _BASIC_CONCUNNENCY_THREADSAFE_QUEUE_FG_HPP_
 
+#include <condition_variable>
 #include <memory>
 #include <mutex>
 namespace concurrency {
@@ -118,11 +119,6 @@ public:
   threadsafe_queue_fg(threadsafe_queue_fg const &other) = delete;
   threadsafe_queue_fg &operator=(threadsafe_queue_fg const &other) = delete;
 
-  std::shared_ptr<T> try_pop() {
-    std::unique_ptr<node> old_head = pop_head();
-    return old_head ? old_head->data : std::shared_ptr<T>();
-  }
-
   void push(T new_value) {
     std::shared_ptr<T> new_data = std::make_shared<T>(new_value);
     std::unique_ptr<node> p = std::make_unique<node>();
@@ -135,7 +131,117 @@ public:
     tail->next = std::move(p);
     tail = new_tail;
   }
+
+  std::shared_ptr<T> try_pop() {
+    std::unique_ptr<node> old_head = pop_head();
+    return old_head ? old_head->data : std::shared_ptr<T>();
+  }
 };
+
+template <typename T> class threadsafe_queue_fg_done {
+private:
+  struct node {
+    std::shared_ptr<T> data;
+    std::unique_ptr<node> next;
+  };
+
+  std::mutex head_mutex;
+  std::unique_ptr<node> head;
+  std::mutex tail_mutex;
+  node *tail;
+  std::condition_variable data_cond;
+
+  node *get_tail() {
+    std::lock_guard<std::mutex> tail_lock(tail_mutex);
+    return tail;
+  }
+
+  std::unique_ptr<node> pop_head() {
+    std::unique_ptr<node> old_head = std::move(head);
+    head = std::move(old_head->next);
+    return old_head;
+  }
+
+  std::unique_lock<std::mutex> wait_for_data() {
+    std::unique_lock<std::mutex> head_lock(head_mutex);
+    // 通知后的解锁条件为队列中存在数据
+    data_cond.wait(head_lock, [&]() { return head.get() != get_tail(); });
+    return std::move(head_lock);
+  }
+
+  std::unique_ptr<node> wait_pop_head() {
+    std::unique_lock<std::mutex> head_lock(wait_for_data());
+    return pop_head();
+  }
+
+  std::unique_ptr<node> wait_pop_head(T &value) {
+    std::unique_lock<std::mutex> head_lock(wait_pop_head());
+    value = std::move(*head->data);
+    return pop_head();
+  }
+
+  std::unique_ptr<node> try_pop_head() {
+    std::lock_guard<std::mutex> lock_head(head_mutex);
+    if (head.get() == get_tail()) {
+      return std::unique_ptr<node>();
+    }
+    return pop_head();
+  }
+
+  std::unique_ptr<node> try_pop_head(T &value) {
+    std::lock_guard<std::mutex> lock_head(head_mutex);
+    if (head.get() == get_tail()) {
+      return std::unique_ptr<node>();
+    }
+    value = std::move(*head->data);
+    return pop_head();
+  }
+
+public:
+  threadsafe_queue_fg_done()
+      : head(std::make_unique<node>()), tail(head.get()) {}
+  threadsafe_queue_fg_done(threadsafe_queue_fg_done const &other) = delete;
+  threadsafe_queue_fg_done &
+  operator=(threadsafe_queue_fg_done const &other) = delete;
+
+  void push(T new_value) {
+    std::shared_ptr<T> new_data = std::make_shared<T>(std::move(new_value));
+    std::unique_ptr<node> p = std::make_unique<node>(); // new dummy node
+    {
+      std::lock_guard<std::mutex> tail_lock(tail_mutex);
+      node *const new_tail = p.get();
+      tail->data = new_data;
+      tail->next = std::move(p);
+      tail = new_tail;
+    }
+    data_cond.notify_one();
+  }
+
+  std::shared_ptr<T> try_pop() {
+    std::unique_ptr<node> old_head = try_pop_head();
+    return old_head ? old_head->data : std::shared_ptr<T>();
+  }
+
+  bool try_pop(T &value) {
+    std::unique_ptr<node> old_head = try_pop_head(value);
+    return old_head;
+  }
+
+  std::shared_ptr<T> wait_and_pop() {
+    std::unique_ptr<node> const old_head = wait_pop_head();
+    return old_head->data;
+  }
+
+  void wait_and_pop(T &value) {
+    std::unique_ptr<node> const old_head = wait_pop_head(value);
+  }
+
+  bool empty() {
+    std::lock_guard<std::mutex> lock_head(head_mutex);
+    return head.get == get_tail();
+  }
+};
+
 } // namespace concurrency
 
 #endif
