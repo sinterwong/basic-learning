55e8e31d91661c85d2b559834de5f7e0a26cf658
Sinter Wong <sintercver@163.com>
2024-02-02 10:14:06 +0800

    add initialized weighted graph

--- src/algo_and_ds/graph/weighted_dense_graph.hpp
+++ src/algo_and_ds/graph/weighted_dense_graph.hpp
@@ -8,40 +8,54 @@
  * @copyright Copyright (c) 2024
  *
  */
-#ifndef __AADS_DENSE_GRAPH_HPP_
-#define __AADS_DENSE_GRAPH_HPP_
+#ifndef __AADS_WEIGHT_DENSE_GRAPH_HPP_
+#define __AADS_WEIGHT_DENSE_GRAPH_HPP_
 
 #include <cassert>
 #include <iostream>
+#include <memory>
 #include <vector>
 
+#include "weighted_edge.hpp"
+
 namespace algo_and_ds::graph {
-class DenseGraph {
-private:
+template <typename Weight> class DenseGraph {
+
+  using weight_edge = Edge<Weight>;
+  using weight_edge_ptr = std::shared_ptr<weight_edge>;
+
   int n, m; // 图的节点数和边数
 
   bool isDirected;
-  std::vector<std::vector<bool>> graph;
+  std::vector<std::vector<weight_edge_ptr>> graph;
 
 public:
   DenseGraph(int n, bool isDirected) : n(n), isDirected(isDirected) {
     // 初始化邻接矩阵
     for (int i = 0; i < n; i++) {
-      graph.push_back(std::vector<bool>(n, false));
+      graph.push_back(std::vector<weight_edge_ptr>(n, nullptr));
     }
   }
+
+  ~DenseGraph() {}
 
   int V() { return n; }
   int E() { return m; }
 
-  void addEdge(int v, int w) {
+  void addEdge(int v, int w, Weight wt) {
     // 因为实现的是简单图，所以需要避免掉平行边
     if (hasEdge(v, w)) {
-      return;
+      // 删除之前的边
+      graph[v][w] = nullptr;
+      if (!isDirected) {
+        graph[w][v] = nullptr;
+      }
+      m--;
     }
-    graph[v][w] = true;
+
+    graph[v][w] = std::make_shared<weight_edge>(v, w, wt);
     if (!isDirected) {
-      graph[w][v] = true;
+      graph[w][v] = std::make_shared<weight_edge>(v, w, wt);
     }
     // 有向图的话如果两个节点之间互相连接则认为是两个边
     m++;
@@ -50,7 +64,7 @@ public:
   bool hasEdge(int v, int w) {
     assert(v >= 0 && v < n);
     assert(w >= 0 && w < n);
-    return graph[v][w];
+    return graph[v][w] != nullptr;
   }
 
   void removeAllParallelEdges() {}
@@ -59,7 +73,12 @@ public:
     std::cout << "adjacent matrix: " << std::endl;
     for (int i = 0; i < V(); i++) {
       for (int j = 0; j < V(); j++) {
-        std::cout << graph[i][j] << ", ";
+        if (graph[i][j]) {
+          std::cout << graph[i][j]->wt() << ", ";
+        } else {
+          std::cout << "null"
+                    << ", ";
+        }
       }
       std::cout << std::endl;
     }
@@ -74,18 +93,18 @@ public:
   public:
     adjIterator(DenseGraph &g, int _v) : G(g), v(_v), index(-1) {}
 
-    int begin() {
+    weight_edge_ptr begin() {
       index = -1;
       return next();
     }
 
-    int next() {
+    weight_edge_ptr next() {
       for (index += 1; index < G.V(); index++) {
         if (G.graph[v][index]) {
-          return index;
+          return G.graph[v][index];
         }
       }
-      return -1;
+      return nullptr;
     }
 
     bool end() { return index >= G.V(); }
---
+++ src/algo_and_ds/graph/weighted_sparse_graph.hpp
@@ -0,0 +1,111 @@
+/**
+ * @file sparse_graph.hpp
+ * @author Sinter Wong (sintercver@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-02-01
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef __AADS_WEIGHT_SPARSE_GRAPH_HPP_
+#define __AADS_WEIGHT_SPARSE_GRAPH_HPP_
+
+#include "weighted_edge.hpp"
+#include <cassert>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+namespace algo_and_ds::graph {
+template <typename Weight> class SparseGraph {
+
+  using weight_edge = Edge<Weight>;
+  using weight_edge_ptr = std::shared_ptr<weight_edge>;
+
+  int n, m; // 图的节点数和边数
+
+  bool isDirected; // 是否是有向图
+
+  std::vector<std::vector<weight_edge_ptr>> graph;
+
+public:
+  SparseGraph(int n, bool isDirected) : n(n), isDirected(isDirected) {
+    // 初始化邻接矩阵
+    for (int i = 0; i < n; i++) {
+      graph.push_back(std::vector<weight_edge_ptr>());
+    }
+  }
+
+  int V() { return n; }
+  int E() { return m; }
+
+  void addEdge(int v, int w, Weight wt) {
+    // 此处没有避免掉平行边，因为判断是否存在平行边的时间复杂度是O(n)
+    // if (hasEdge(v, w)) {
+    //   return;
+    // }
+
+    assert(v >= 0 && v < n);
+    assert(w >= 0 && w < n);
+
+    graph[v].push_back(std::make_shared<weight_edge>(v, w, wt));
+    // v != w 解决环边问题（简单图不考虑环边和平行边）
+    if (v != w && !isDirected) {
+      graph[w].push_back(std::make_shared<weight_edge>(v, w, wt));
+    }
+    m++;
+  }
+
+  bool hasEdge(int v, int w) {
+    assert(v >= 0 && v < n);
+    assert(w >= 0 && w < n);
+    for (int i = 0; i < graph[v].size(); i++) {
+      if (graph[v][i]->other(v) == w) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void show() {
+    for (int i = 0; i < V(); i++) {
+      std::cout << "vertex " << i << ": ";
+      for (int j = 0; j < graph[i].size(); j++) {
+
+        std::cout << "( to: " << graph[i][j]->w() << ",wt:" << graph[i][j]->wt()
+                  << "), ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+public:
+  class adjIterator {
+    SparseGraph &G;
+    int v;     // 关注的顶点
+    int index; // 需要维护的索引
+
+  public:
+    adjIterator(SparseGraph &g, int _v) : G(g), v(_v), index(0) {}
+
+    weight_edge_ptr begin() {
+      index = 0;
+      return G.graph[v][index];
+    }
+
+    weight_edge_ptr next() {
+      index++;
+      if (index < G.graph[v].size()) {
+        return G.graph[v][index];
+      }
+      return nullptr;
+    }
+
+    bool end() { return index >= G.graph[v].size(); }
+  };
+};
+} // namespace algo_and_ds::graph
+
+#endif
